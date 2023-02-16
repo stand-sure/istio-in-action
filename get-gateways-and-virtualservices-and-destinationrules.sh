0420128eb5807bf05932c9674777f20c627ba0b9
C. J. Anderson <chris@standsure.io>
2023-02-16 14:32:13 -0500

    updated

--- get-gateways-and-virtualservices-and-destinationrules.sh
+++ get-gateways-and-virtualservices-and-destinationrules.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-kubectl get gateways --output yaml | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
-kubectl get virtualservices --output yaml | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
-kubectl get destinationrules --output yaml | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
+kubectl get gateways --output yaml "$@" | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
+kubectl get virtualservices --output yaml "$@" | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
+kubectl get destinationrules --output yaml "$@" | yq ' .items[] | { "name": .metadata.name, "kind": .kind, "spec": .spec }'
 
